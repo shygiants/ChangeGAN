9735edd3a0f19451378aac94ff2c20eef6dad9c3
윤상훈 <shygiants@gmail.com>
2017-07-26 23:37:30 +0900

    Compare results between models

--- browser/src/App.js
+++ browser/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import './assets/react-toolbox/theme.css';
 import Slider from 'react-toolbox/lib/slider/Slider';
+import Tab from 'react-toolbox/lib/tabs/Tab';
+import Tabs from 'react-toolbox/lib/tabs/Tabs';
 
 import theme from './assets/react-toolbox/theme'
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
@@ -10,13 +12,20 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      step: 78375,
-      batch: 1
+      step: 100002,
+      batch: 1,
+      fixedIndex: '0',
     };
 
-    this.batchSize = 50
+    this.batchSize = 50;
+    this.stepMap = {
+      '0': 100002,
+      '1': 90030,
+      '2': 78375,
+    }
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleFixedTabChange = this.handleFixedTabChange.bind(this);
   }
 
   handleChange(value) {
@@ -25,10 +34,22 @@ class App extends Component {
     });
   }
 
+  handleFixedTabChange(index) {
+    this.setState({
+      step: this.stepMap[index],
+      fixedIndex: index
+    });
+  }
+
   render() {
     return (
       <ThemeProvider theme={theme}>
         <section>
+          <Tabs index={this.state.fixedIndex} onChange={this.handleFixedTabChange} fixed>
+            <Tab label='Naive ChangeGAN'><small></small></Tab>
+            <Tab label='ChangeGAN with bbox and color loss'><small></small></Tab>
+            <Tab label='ChangeGAN without bbox'><small></small></Tab>
+          </Tabs>
           <div className="slider">
             <Slider pinned snaps min={1} max={10} step={1} editable value={this.state.batch} onChange={this.handleChange} />  
           </div>
